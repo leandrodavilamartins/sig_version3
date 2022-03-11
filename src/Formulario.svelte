73427097d2b00ce45f53e969f8a08e78fc541689
Leandro D'Ávila Martins Braga <leandrodavilamartins@gmail.com>
2022-03-10 23:14:23 -0300

    first commit

--- src/Formulario.svelte
+++ src/Formulario.svelte
@@ -1,48 +1,105 @@
 <script>
-  import { produto } from "./store";
-  let nome;
-  let preço;
-  let data;
+  //import { produto } from "./store";
+  let nome = "";
+  let preço = null;
+  let data = "";
+  let fornecedor = "";
+  let isValid = false;
+  let ingrediente = "";
 
-  //$: console.log(nome);
-  //$: console.log(preço);
-  $: console.log(data);
-  function saveData() {
-    let year = data.slice(0, 4);
-    let month = parseInt(data.slice(5, 7)) - 1; // there is a problem if the month is 0
-    let day = data.slice(8, 10);
-    let d = new Date(year, month, day);
-    //let timestamp = d.getTime();
-    $produto = [...$produto, { nome: nome, preço: preço }];
-    db.collection("cotacoes")
-      .doc()
-      .set({
-        nome: nome,
-        preço: parseFloat(preço),
-        data: d,
-      })
-      .then((res) => {
-        console.log("Cotação salva com sucesso!");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  // validação reativa
+  $: if (nome !== "" && preço !== null && fornecedor !== "") {
+    isValid = true;
+    console.log(isValid);
+  } else {
+    isValid = false;
+    console.log(isValid);
   }
-  function logger() {
+  $: console.log(nome);
+  $: console.log(preço);
+  $: console.log(data);
+  $: console.log(isValid);
+  $: console.log(ingrediente);
+  $: console.log(fornecedor);
+
+  async function getEstoqueData() {
+    const resposta = await db.collection("estoque").get();
+    const docs = await resposta.docs;
+    const res = await docs.map((doc) => {
+      return doc.data();
+    });
+    //console.log(res);
+    return res;
+  }
+
+  async function getFornecedoresData() {
+    const resposta = await db.collection("fornecedores").get();
+    const docs = await resposta.docs;
+    const res = await docs.map((doc) => {
+      return doc.data();
+    });
+    return res;
+  }
+
+  function saveData() {
+    if (!isValid) {
+      console.log("Dados inválidos");
+    } else {
+      let year = data.slice(0, 4);
+      let month = parseInt(data.slice(5, 7)) - 1; // there is a problem if the month is 0
+      let day = data.slice(8, 10);
+      let d = new Date(year, month, day);
+      //let timestamp = d.getTime();
+      //$produto = [...$produto, { nome: nome, preço: preço }];
+      db.collection("cotacoes")
+        .doc()
+        .set({
+          nome: nome,
+          preço: preço,
+          data: d,
+          fornecedor: fornecedor,
+        })
+        .then((res) => {
+          console.log("Cotação salva com sucesso!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+  /*   function logger() {
     let year = data.slice(0, 4);
     let month = parseInt(data.slice(5, 7)) - 1;
     let day = data.slice(8, 10);
     let d = new Date(year, month, day);
-    let timestamp = d.getTime();
-  }
+    //let timestamp = d.getTime();
+  } */
+
+  let dbEstoqueData = getEstoqueData();
+  let dbFornecedoresData = getFornecedoresData();
 </script>
 
 <div>
   <p />
-  <p>Nome do item</p>
-  <input type="text" bind:value={nome} />
+  {#await dbEstoqueData then myData}
+    <select bind:value={nome}>
+      <option selected>Escolha</option>
+      {#each myData as d}
+        <option value={d.ingrediente}>{d.ingrediente}</option>
+      {/each}
+    </select>
+  {/await}
   <p>Preço</p>
-  <input type="text" bind:value={preço} />
+  <input type="number" bind:value={preço} />
+  <p>Fornecedor</p>
+  <select bind:value={fornecedor}>
+    {#await dbFornecedoresData then myData}
+      <option selected>Escolha</option>
+      {#each myData as d}
+        <option value={d.nome}>{d.nome}</option>
+      {/each}
+    {/await}
+  </select>
   <p>Data</p>
   <input type="date" bind:value={data} />
   <button on:click={saveData}>Salvar Cotação</button>
